a2cc2146fcbb065d54d6bd0a43b91ec3471d2697
ilyapatis24 <ilyapatis24@mail.ru>
2024-05-26 22:33:37 +0300

    initial commit

---
+++ 01/main.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <thread>
+
+int maxClients = 5;
+int clientCounter = 0;
+
+void clientThread() {
+    while (clientCounter < maxClients)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        clientCounter++;
+        std::cout << "Клиент встал в очередь, текущее число клиентов: " << clientCounter << std::endl;
+    }
+}
+
+void operatorThread() {
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        if (clientCounter > 0)
+        {
+            clientCounter--;
+            std::cout << "Клиент обслужен, текущее число клиентов: " << clientCounter << std::endl;
+        }
+        else
+        {
+            std::cout << "Оператор закончил работу, текущее число клиентов: " << clientCounter << std::endl;
+            break;
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    system("chcp 1251");
+    std::thread client(clientThread);
+    std::thread operatorT(operatorThread);
+
+    client.join();
+    operatorT.join();
+
+    return 0;
+}
